f3517f6f0c56d11e6ae02aae580661f05a99adcc
Kenzor987 <kegille@a112pc09L.local.isima.fr>
2021-10-25 21:31:34 +0200

    take-good une marchandise presque fini

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -17,4 +17,25 @@ router.post("/", function (req, res) {
 router.get("/", function (req, res) {
   res.json(databaseService.getGames())
 })
+
+// PUT
+router.put("/:id/take-good/", function (req, res) {
+  const gameId = parseInt(req.params.id)
+  const playerId = parseInt(req.header("playerIndex"))
+  if ((playerId === 0 || playerId === 1) && gameId === 0) {
+    return res.status(400).send("Missing path and/or header parameters")
+  } else {
+    const game = databaseService.getGames()[gameId]
+    if (playerId === game.currentPlayerIndex) {
+      const playerHand = game._players[playerId].hand
+      if (playerHand.length < 7) {
+        playerHand.forEach((element) => console.log(element))
+        playerHand.push(req.body.good)
+        playerHand.forEach((element) => console.log(element))
+      }
+      return res.status(200).send()
+    }
+  }
+})
+// TO DO les exceptions si le joueur a plus de 7 cartes et si ce n'est pas au playerId de jouer
 export default router
